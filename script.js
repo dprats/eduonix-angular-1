706c87b2af3f171f1aafcb034351bdf2dc59ddb6
Diego Prats <diego.prats@gmail.com>
2015-08-01 16:56:22 -0700

    finish tutorial

--- script.js
+++ script.js
@@ -9,13 +9,45 @@ var app = angular.module('computer', ['ngRoute'])
 			templateUrl: 'main.html',
 			controller: 'MainCtrl'
 		}).
+		when('/about', {
+			templateUrl: 'about.html',
+			controller: 'MainCtrl'
+		}).
+		when('/services', {
+			templateUrl: 'services.html',
+			controller: 'ServicesCtrl'
+		}).
+		when('/contact', {
+			templateUrl: 'contact.html',
+			controller: 'ContactCtrl'
+		}).
 		otherwise({
 			redirecTo: '/main'
 		});
 
 }])
-.controller('MainCtrl',['$scope',function($scope){
-	$scope.person = 'John Doe';
+.controller('MainCtrl',['$scope', '$http', function($scope, $http){
 	console.log($scope);
+	$http.get('services.json').then(function(response){
+		$scope.services = response.data;
+	});
 
+}])
+.controller('ServicesCtrl', ['$scope','$http', function($scope,$http){
+
+	console.log('welcome to Services');
+
+	$http.get('services.json').then(function(response){
+		$scope.services = response.data;
+		console.log('services data:');
+		console.log($scope.services);
+	});
+
+
+}])
+.controller('ContactCtrl', ['$scope','$http', function($scope, $http){
+	
+	$http.get('locations.json').then(function(response){
+		$scope.locations = response.data;
+	});
 }]);
